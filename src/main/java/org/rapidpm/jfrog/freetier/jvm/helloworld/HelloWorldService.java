ce11445ee10b5ea38df7d9cd5c83463b47ea273b
Sven Ruppert <sven.ruppert@gmail.com>
2020-08-26 12:53:58 +0200

    pipelines stuff

--- src/main/java/org/rapidpm/jfrog/freetier/jvm/helloworld/HelloWorldService.java
+++ src/main/java/org/rapidpm/jfrog/freetier/jvm/helloworld/HelloWorldService.java
@@ -3,6 +3,6 @@ package org.rapidpm.jfrog.freetier.jvm.helloworld;
 public class HelloWorldService {
 
   public String toUpperCase(String input){
-    return input.toUpperCase();
+    return input.toUpperCase() + "";
   }
 }
